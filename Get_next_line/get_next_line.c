04cb627a0702261a6ffad417d9196448511b953d
Hugo CORRALES <hcorrale@e2r11p12.42.fr>
2016-01-20 16:55:24 +0100

    modif get_next_line

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hcorrale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/12 14:53:02 by hcorrale          #+#    #+#             */
-/*   Updated: 2016/01/19 17:40:56 by hcorrale         ###   ########.fr       */
+/*   Updated: 2016/01/20 16:46:14 by hcorrale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,33 @@ static int		ft_len(t_clist *lst)
 		i++;
 	}
 	return (i);
+}
+
+static t_clist	*ft_pushlst(t_clist *begin, char c, int fd, int n)
+{
+	t_clist		list;
+	t_clist		rev;
+
+	list = (t_clist *)malloc(sizeof(t_clist));
+	if (!list)
+		return (NULL);
+	list->fd = fd;
+	list->c = c;
+	if (!begin)
+		list->next = NULL;
+	else
+		list->next = begin;
+	if (n == 0)
+	{
+		rev = NULL;
+		while (list)
+		{
+			rev = ft_free_list(rev, list->c, fd, 1);
+			free(&list);
+		}
+		return (rev);
+	}
+	return (list);
 }
 
 static t_clist	*ft_read(int fd);
