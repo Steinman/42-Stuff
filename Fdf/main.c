f42aeb2162069452088ca2d6eab8b8b7a5455f04
Hugo CORRALES <hcorrale@e2r10p20.42.fr>
2016-03-14 17:09:19 +0100

    modif main

--- Fdf/main.c
+++ Fdf/main.c
@@ -6,7 +6,7 @@
 /*   By: hcorrale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 15:53:56 by hcorrale          #+#    #+#             */
-/*   Updated: 2016/03/10 16:11:01 by hcorrale         ###   ########.fr       */
+/*   Updated: 2016/03/14 16:59:58 by hcorrale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,33 @@
 #include "mlx.h"
 #include "libft/libft.h"
 
+static int		*ft_inttab(char *buf)
+{
+	int			*tab;
+	int			j;
+	int i;
+
+	tab = (int *)malloc(sizeof(int));
+	j = 0;
+	i = 0;
+	while (buf[i] != '\0')
+	{
+		if (buf[i] == ' ' || buf[i] == '\n')
+			i++;
+		else
+		{
+			tab[j] = ft_atoi_nb(buf, i);
+			i++;
+			j++;
+		}
+	}
+	return (tab);
+}
+
 int				main(int argc, char **argv)
 {
 	int			fd;
+	int			*tab;
 	char 		*line;
 	char		*buf;
 	size_t		i;
@@ -37,6 +61,8 @@ int				main(int argc, char **argv)
 	while (buf[i] != '\0')
 		i++;
 	buf[i - 1] = '\0';
-	printf("%s", buf);
+	printf("%s\n", buf);
+	tab = ft_inttab(buf);
+	printf("%d", tab[42]);
 	return (0);
 }
