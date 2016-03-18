b89e9721f7c7c01c47b8dfaf8fc9df7880ec9ab4
Hugo CORRALES <hcorrale@e2r10p20.42.fr>
2016-03-18 17:55:51 +0100

    modif main again

--- Fdf/main.c
+++ Fdf/main.c
@@ -6,10 +6,9 @@
 /*   By: hcorrale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 15:53:56 by hcorrale          #+#    #+#             */
-/*   Updated: 2016/03/15 14:35:04 by hcorrale         ###   ########.fr       */
+/*   Updated: 2016/03/18 17:33:41 by hcorrale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <fcntl.h>
 #include <stdlib.h>
@@ -17,52 +16,57 @@
 #include "mlx.h"
 #include "libft/libft.h"
 
-static int		*ft_inttab(char *buf)
+static int		**ft_inttab(char **buf, int h)
 {
-	int			*tab;
+	int			**tab;
 	int			j;
-	static int	i;
+	int			i;
+	int			k;
 
-	tab = (int *)malloc(sizeof(int));
+	tab = (int **)malloc(sizeof(int) * h);
 	j = 0;
 	i = 0;
+	k = 0;
 	while (buf[i] != '\0')
 	{
-		if (buf[i] == ' ' || buf[i] == '\n')
+		tab[k] = (int *)malloc(sizeof(int));
+		if (buf[i] == ' ')
 			i++;
-		else
-		{
-			tab[j] = ft_atoi_nb(buf, &i);
-			printf("%d", tab[j]);
-			i++;
-			j++;
-		}
+		tab[k][j] = ft_atoi_nb(buf, i);
+		i++;
+		j++;
 	}
+	tab[k] = NULL;
 	return (tab);
 }
 
 int				main(int argc, char **argv)
 {
 	int			fd;
-	int			*tab;
+	int			**tab;
 	char 		*line;
 	char		*buf;
+	char		**stock;
 	size_t		i;
+	int			j;
 
 	if (argc != 2)
 		return (0);
+	j = 0;
 	fd = open(argv[1], O_RDONLY);
 	buf = ft_strnew(1);
 	while (get_next_line(fd, &line) > 0)
 	{
 		buf = ft_strjoin(buf, line);
 		buf = ft_strjoin(buf, "\n");
+		j++;
 	}
 	close(fd);
 	while (buf[i] != '\0')
 		i++;
 	buf[i - 1] = '\0';
 	printf("%s\n", buf);
-	tab = ft_inttab(buf);
+	stock = ft_strsplit(buf, '\n');
+	tab = ft_inttab(stock, j);
 	return (0);
 }
