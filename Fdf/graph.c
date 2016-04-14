8d03bb645545538e2bb203ad1cc01a1d12cab85c
Hugo CORRALES <hcorrale@e2r10p18.42.fr>
2016-04-14 17:46:00 +0200

    ajout fonction draw_tab

--- Fdf/graph.c
+++ Fdf/graph.c
@@ -6,7 +6,7 @@
 /*   By: hcorrale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/11 13:56:33 by hcorrale          #+#    #+#             */
-/*   Updated: 2016/04/13 17:58:15 by hcorrale         ###   ########.fr       */
+/*   Updated: 2016/04/14 17:16:34 by hcorrale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,22 +37,41 @@ void		ft_draw_line(t_point a, t_point b, void *mlx, void *win)
 void		ft_drawtab(t_var v)
 {
 	int		i;
+	int		mul;
 
+	mul = 300;
 	i = 0;
+	while ((v.len * mul) > (v.winx - 200))
+			mul--;
+	v.a.x = 100;
+	v.a.y = v.winy / 2;
+	v.b.x = v.a.x + mul / 2;
+	v.b.y = v.a.y - mul / 2;
 	while (i < v.len)
 	{
-		
+		ft_draw_line(v.a, v.b, v.mlx, v.win);
+		v.b.y += mul;
+		ft_draw_line(v.a, v.b, v.mlx, v.win);
+		v.a.x += mul / 2;
+		v.a.y -= mul / 2;
+		v.b.x += mul / 2;
+		v.b.y -= (mul + mul / 2);
+		i++;
 	}
+	v.b.y += mul;
+	ft_draw_line(v.a, v.b, v.mlx, v.win);
 }
 
 int			main(void)
 {
 	t_var	v;
+
+	v.len = 19;
 	v.winx = 800;
 	v.winy = 800;
-
-	v->mlx = mlx_init();
-	v->win = mlx_new_window(v->mlx, v.winx, v.winy, "mlx_win");
-	mlx_loop(mlx);
+	v.mlx = mlx_init();
+	v.win = mlx_new_window(v.mlx, v.winx, v.winy, "mlx_win");
+	ft_drawtab(v);
+	mlx_loop(v.mlx);
 	return (0);
 }
