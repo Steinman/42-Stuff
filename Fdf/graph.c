f9f8db61672088a6fa39ca65df8895650e8ebb1d
Hugo CORRALES <hcorrale@e2r10p20.42.fr>
2016-05-02 17:01:51 +0200

    t_var is now *t_var

--- Fdf/graph.c
+++ Fdf/graph.c
@@ -6,7 +6,7 @@
 /*   By: hcorrale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/11 13:56:33 by hcorrale          #+#    #+#             */
-/*   Updated: 2016/05/02 16:41:49 by hcorrale         ###   ########.fr       */
+/*   Updated: 2016/05/02 17:01:17 by hcorrale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <math.h>
 
-static void	ft_draw_line(t_point a, t_point b, t_var v)
+static void	ft_draw_line(t_point a, t_point b, t_var *v)
 {
 	double	x0;
 	double	y0;
@@ -29,42 +29,42 @@ static void	ft_draw_line(t_point a, t_point b, t_var v)
 	{
 		x0 = a.x + (dx * tx);
 		y0 = a.y + (dy * tx);
-		mlx_pixel_put(v.mlx, v.win, x0, y0, 0x00FFFFFF);
+		mlx_pixel_put(v->mlx, v->win, x0, y0, 0x00FFFFFF);
 		tx += 1. / sqrtf((dx * dx) + (dy * dy));
 	}
 }
 
-static void	ft_drawtab(t_var v, int j)
+static void	ft_drawtab(t_var *v, int j)
 {
 	int		i;
 
 	i = 0;
-	v.a.x = ((v.winx - (v.len * (v.mul / 2))) / 2) - ((v.l * (v.mul / 2)) / 2) + j * (v.mul / 2);
-	v.a.y = v.winy / 2 + j * (v.mul / 2);
-	v.b.x = v.a.x + v.mul / 2;
-	v.b.y = v.a.y - v.mul / 2;
-	while (i < v.len)
+	v->a.x = ((v->winx - (v->len * (v->mul / 2))) / 2) - ((v->l * (v->mul / 2)) / 2) + j * (v->mul / 2);
+	v->a.y = v->winy / 2 + j * (v->mul / 2);
+	v->b.x = v->a.x + v->mul / 2;
+	v->b.y = v->a.y - v->mul / 2;
+	while (i < v->len)
 	{
-		v.c.x = v.b.x;
-		if (j < v.l - 1)
-			v.c.y = v.a.y + (v.mul / 2) - v.tab[j + 1][i];
-		v.a.y -= v.tab[j][i];
-		v.b.y -= v.tab[j][i + 1];
-		if (i < v.len - 1)
-			ft_draw_line(v.a, v.b, v);
-		if (j < v.l - 1)
-			ft_draw_line(v.a, v.c, v);
-		v.a.y += v.tab[j][i];
-		v.b.y += v.tab[j][i + 1];
-		v.a.x += v.mul / 2;
-		v.a.y -= v.mul / 2;
-		v.b.x += v.mul / 2;
-		v.b.y -= v.mul / 2;
+		v->c.x = v->b.x;
+		if (j < v->l - 1)
+			v->c.y = v->a.y + (v->mul / 2) - v->tab[j + 1][i];
+		v->a.y -= v->tab[j][i];
+		v->b.y -= v->tab[j][i + 1];
+		if (i < v->len - 1)
+			ft_draw_line(v->a, v->b, v);
+		if (j < v->l - 1)
+			ft_draw_line(v->a, v->c, v);
+		v->a.y += v->tab[j][i];
+		v->b.y += v->tab[j][i + 1];
+		v->a.x += v->mul / 2;
+		v->a.y -= v->mul / 2;
+		v->b.x += v->mul / 2;
+		v->b.y -= v->mul / 2;
 		i++;
 	}
 }
 
-int			ft_zoom(int keycode, t_var v)
+int			ft_zoom(int keycode, t_var *v)
 {
 	int		i;
 
@@ -72,8 +72,8 @@ int			ft_zoom(int keycode, t_var v)
 	printf("key event %d\n", keycode);
 	if (keycode == 126)
 	{
-		v.mul = v.mul * 2;
-		while (i < v.l)
+		v->mul = v->mul * 2;
+		while (i < v->l)
 		{
 			ft_drawtab(v, i);
 			i++;
@@ -82,8 +82,8 @@ int			ft_zoom(int keycode, t_var v)
 	}
 	if (keycode == 125)
 	{
-		v.mul = v.mul / 2;
-		while (i < v.l)
+		v->mul = v->mul / 2;
+		while (i < v->l)
 		{
 			ft_drawtab(v, i);
 			i++;
@@ -95,31 +95,31 @@ int			ft_zoom(int keycode, t_var v)
 
 int			main(int ac, char **av)
 {
-	t_var	v;
+	t_var	*v;
 	int		i;
 
 	i = 0;
-	v.err = 0;
+	v->err = 0;
 	if (ac != 2)
 		return (-1);
 	else
 	{
-		if ((v = ft_open(av[1], v, 0)).err == -1)
+		if ((v = ft_open(av[1], *v, 0))->err == -1)
 			ft_putstr("wallah sa march pa");
 	}
-	v.winx = 800;
-	v.winy = 800;
-	v.mlx = mlx_init();
-	v.win = mlx_new_window(v.mlx, v.winx, v.winy, "mlx_win");
-	v.mul = 300;
-	while ((v.len * v.mul) > (v.winx - 200))
-		v.mul--;
-	while (i < v.l)
+	v->winx = 800;
+	v->winy = 800;
+	v->mlx = mlx_init();
+	v->win = mlx_new_window(v->mlx, v->winx, v->winy, "mlx_win");
+	v->mul = 300;
+	while ((v->len * v->mul) > (v->winx - 200))
+		v->mul--;
+	while (i < v->l)
 	{
 		ft_drawtab(v, i);
 		i++;
 	}
-	mlx_key_hook(v.win, ft_zoom, &v);
-	mlx_loop(v.mlx);
+	mlx_key_hook(v->win, ft_zoom, &v);
+	mlx_loop(v->mlx);
 	return (0);
 }
